8cac4a7385be1775eff678c2c7d722823c9d46d4
xbl <1546766776@qq.com>
2019-10-12 23:02:16 +0800

    补充

---
+++ 小程序demo/pages/message/message.js
@@ -0,0 +1,77 @@
+// pages/message/message.js
+Page({
+  data: {
+    msg: "hello message",
+    num: 1000,
+    flag: true,
+    person: {
+      name: "小白",
+      age: 18,
+      hobby: "敲代码"
+    },
+    id: "这是一则信息",
+    arr: ["小红", "小白", "小黑"],
+    student: {
+      name: "ddt",
+      age: 18,
+      sex: "girl"
+    },
+    myColor:"pink"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
